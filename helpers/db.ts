ad12d913c33a996019319890cd3330ea2964e5ae
PawelMike <michalkowski.pawel@gmail.com>
2025-11-05 14:50:35 +0100

    dotenv

--- helpers/db.ts
+++ helpers/db.ts
@@ -1,11 +1,12 @@
 import { Client } from 'pg';
+import 'dotenv/config';
 
 const client = new Client({
-    user: 'postgres',
-    password: 'Test1234',
-    host: '127.0.0.1',
+    user: process.env.CLIENT_USER,
+    password: process.env.CLIENT_PASSWORD,
+    host: process.env.CLIENT_HOST,
     port: 5432,
-    database: 'postgres',
+    database: process.env.CLIENT_DATABASE,
   })
 
 export async function connectToDatabase() {
